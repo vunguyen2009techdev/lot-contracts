b1c6b36e8619ef02950a7ede797b729c63aaae01
vu.nguyen <anhvu.hcmus.2012@gmail.com>
2021-11-24 14:39:45 +0700

    edit SC

--- contracts/LandNFTCrowdsale.sol
+++ contracts/LandNFTCrowdsale.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
@@ -11,13 +12,8 @@ contract LandNFTCrowdsale is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20Metadata;
 
     IERC20Metadata public immutable paymentToken;
+    uint256 public tokenUnit;
     LandNFT public immutable nft;
-
-    uint8 public constant PARCEL_1x1 = 1;
-    uint8 public constant PARCEL_5x5 = 5;
-    uint8 public constant PARCEL_8x8 = 8;
-    uint8 public constant PARCEL_10x10 = 10;
-    uint8 public constant PARCEL_15x15 = 15;
 
     struct Parcel {
         uint256 price;
@@ -25,18 +21,13 @@ contract LandNFTCrowdsale is Ownable, ReentrancyGuard {
         uint256 supply;
     }
 
-    mapping(uint8 => Parcel) public parcels;
-    mapping(uint256 => bool) public tags;
+    mapping(uint256 => Parcel) public parcels;
+    mapping(uint256 => bool) public itemIds;
 
-    event Buy(
-        address indexed buyer,
-        uint8 indexed parcelId,
-        uint256 indexed tag,
-        uint256 price
-    );
+    event Buy(address indexed buyer, uint256 indexed itemId, uint256 price);
 
-    event UpdateParcel(
-        uint8 indexed parcelId,
+    event ListedItem(
+        uint256 indexed itemId,
         uint256 price,
         uint256 cap,
         uint256 supply
@@ -55,52 +46,52 @@ contract LandNFTCrowdsale is Ownable, ReentrancyGuard {
         nft.grantRole(nft.DEFAULT_ADMIN_ROLE(), owner);
 
         paymentToken = IERC20Metadata(token);
-        uint256 tokenUnit = 10**paymentToken.decimals();
-
-        parcels[PARCEL_1x1] = Parcel(1 * tokenUnit, 23597, 0);
-        parcels[PARCEL_5x5] = Parcel(2 * tokenUnit, 8925 + 15731, 0);
-        parcels[PARCEL_8x8] = Parcel(3 * tokenUnit, 15744 + 23597, 0);
-        parcels[PARCEL_10x10] = Parcel(4 * tokenUnit, 7900 + 11798, 0);
-        parcels[PARCEL_15x15] = Parcel(5 * tokenUnit, 8775 + 3933, 0);
+        tokenUnit = 10**paymentToken.decimals();
     }
 
-    /// @notice Update parcel's sale info
+    /// @notice Listed item's sale info
     /// @dev Supply will be carried on and cannot be overwritten
-    /// @param parcelId Unique identifier per parcel type
-    /// @param price Sale price in the smallest unit i.e. wei
-    /// @param cap Maximum supply
-    function updateParcel(
-        uint8 parcelId,
-        uint256 price,
-        uint256 cap
+    /// @param _itemId Unique identifier per parcel type
+    /// @param _price Sale price in the smallest unit i.e. wei
+    /// @param _cap Maximum supply
+    function listedItem(
+        uint256 _itemId,
+        uint256 _price,
+        uint256 _cap
     ) public onlyOwner {
         // carry on existing supply
-        uint256 currentSupply = parcels[parcelId].supply;
-        parcels[parcelId] = Parcel(price, cap, currentSupply);
-        emit UpdateParcel(parcelId, price, cap, currentSupply);
+        uint256 currentSupply = parcels[_itemId].supply;
+        parcels[_itemId] = Parcel(_price, _cap, currentSupply);
+        emit ListedItem(_itemId, _price, _cap, currentSupply);
     }
 
-    /// @notice Buy a single NFT
-    /// @dev Use tag to associate the purchase order with tokenId
-    /// @param parcelId Unique identifier per parcel type
-    /// @param tag Unique number generated on the front-end
-    function buy(uint8 parcelId, uint256 tag) public nonReentrant {
-        require(!tags[tag], "LandNFT: tag already used");
-        tags[tag] = true;
+    /// @notice Buy a single/multiples NFT
+    /// @dev Use _itemId to associate the purchase order with tokenId
+    /// @param _itemId Unique number generated on the front-end
+    function buy(uint256 _itemId, uint256 quantity) public nonReentrant {
+        require(!itemIds[_itemId], "LandNFT: tag already used");
+        require(quantity > 0, "LandNFT: quantity must from 1 and above");
+        itemIds[_itemId] = true;
 
-        Parcel memory parcel = parcels[parcelId];
+        Parcel memory parcel = parcels[_itemId];
         uint256 price = parcel.price;
-        uint256 supply = parcel.supply;
         uint256 cap = parcel.cap;
+        uint256 supply = parcel.supply;
 
         require(price > 0, "LandNFT: price was not set");
         require(supply < cap, "LandNFT: supply reached cap");
+        require(
+            price % quantity == 0,
+            "LandNFT: price must be corresponding to quantity"
+        );
 
-        parcels[parcelId].supply = supply + 1;
-
-        address buyer = msg.sender;
-        paymentToken.safeTransferFrom(buyer, owner(), price);
-        nft.mint(buyer);
-        emit Buy(buyer, parcelId, tag, price);
+        for (uint256 i = 0; i < quantity; i += 1) {
+            supply += 1;
+            parcels[_itemId].supply = supply;
+            address buyer = msg.sender;
+            paymentToken.safeTransferFrom(buyer, owner(), price / quantity);
+            nft.mint(buyer);
+            emit Buy(buyer, _itemId, price);
+        }
     }
 }
